3dc566c1c6a0469a4c7bd9a8dc2b0c75c9d89124
Natalia <nivanova86@bk.ru>
2022-10-17 04:31:56 +0300

    add 1st task

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,6 +1,23 @@
 ﻿// ДЗ1
 //Задача 2: Напишите программу, которая на вход принимает два числа 
 //и выдаёт, какое число большее, а какое меньшее.
+/*
+
+Console.Write("Input a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b) Console.Write("two numbers are equal");
+
+else
+{
+    int max = a;
+    if (a < b) max = b;
+    Console.Write("max = " + max); 
+}
+
+*/
 
 
 
