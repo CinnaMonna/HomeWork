8f2905ecb5517650260b7e898a8f9b630f6c6a98
Natalia <nivanova86@bk.ru>
2022-10-17 05:00:59 +0300

    add 2st task

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -2,7 +2,6 @@
 //Задача 2: Напишите программу, которая на вход принимает два числа 
 //и выдаёт, какое число большее, а какое меньшее.
 /*
-
 Console.Write("Input a first number: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
@@ -16,8 +15,39 @@ else
     if (a < b) max = b;
     Console.Write("max = " + max); 
 }
+*/
+
+//Задача 2:Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+/*
+int[]array = new int[3];
+
+int i = 0;
+while (i < array.Length)
+{
+    Console.Write("Input a number: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    i +=1;
+}
+
+i = 1;
+int max = array[0];
+
+while (i < array.Length)
+{
+    if (array[i] > max)    max = array[i];
+    i += 1;
+}
+
+Console.Write("max = " + max);
 
 */
+
+
+
+
+
+
 
 
 
