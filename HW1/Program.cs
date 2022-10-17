774c2cf9ab8ee9754787e874f37467c0d852e45b
Natalia <nivanova86@bk.ru>
2022-10-17 05:09:55 +0300

    add 3d task

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -43,6 +43,16 @@ Console.Write("max = " + max);
 
 */
 
+//Задача 3: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write("Input a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2 == 0)  Console.Write("Yes");
+else Console.Write("No");
+*/
+
 
 
 
