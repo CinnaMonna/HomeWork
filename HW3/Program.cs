fe2658d5db08981dad017334d8468a3746d2def3
Natalia <nivanova86@bk.ru>
2022-10-27 04:03:03 +0300

    add 3d task in HW3

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -86,3 +86,24 @@ Console.WriteLine($"distance between points A ({xA},{yA},{zA}) and B ({xB},{yB},
 
 */
 
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+/*
+void ThirdPowersTable(int number)
+{
+    for (int cur = 1; cur <= number; cur++)
+        Console.Write(cur * cur * cur + "  ");
+        
+}
+
+Console.Write("input the number N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"table of third powers of numbers 1 to {num}: ");
+ThirdPowersTable(num);
+
+*/
