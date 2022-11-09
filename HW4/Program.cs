74fbd5b2d1cff65b94d29b15fce8903bcffc58f4
Natalia <nivanova86@bk.ru>
2022-11-10 01:21:48 +0300

    Add 2nd task

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -4,11 +4,18 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+/*
 double RaiseToPower(double num, int pow)
 {
-    double res = num;
-    for(int cur = 2; cur <= pow; cur++)
-        res = res * num;
+    double res;
+    if(pow == 0)
+        res = 1;
+    else
+    {
+        res = num;
+        for(int cur = 2; cur <= pow; cur++)
+            res = res * num;
+    }
     return res;
 }
 
@@ -18,4 +25,49 @@ Console.Write("input a integer power: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"number {a} raised to the power of {b} is {Math.Round(RaiseToPower(a, b), 2)}");
+*/
+
+// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int CountOfDigits(int num)
+{
+    int res;
+    if(num == 0)
+        res = 1;
+    else
+    {
+        res = 0;
+        while(num != 0)
+        {
+            num = num / 10;
+            res++;
+        }
+    }    
+    return res;
+}
+
+
+int SumOfDigits(int num)
+{
+    int digitsCount = CountOfDigits(num);
+    int digit;
+    int sum = 0;
+    
+    while(digitsCount != 0)
+    {
+        digit = num / Convert.ToInt32(Math.Pow(10, digitsCount - 1));
+        sum = sum + digit;
+        num = num % Convert.ToInt32(Math.Pow(10, digitsCount - 1));
+        digitsCount -= 1;
+    }
+    return sum;
+}
+
+Console.Write("input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum of digits of the number {number} is {SumOfDigits(number)}");
 
