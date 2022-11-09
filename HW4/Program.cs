6a670aba04d7d3d37cf1cc7d46447a0cf8eea5ae
Natalia <nivanova86@bk.ru>
2022-11-10 01:45:40 +0300

    Add 3d task

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -33,6 +33,7 @@ Console.WriteLine($"number {a} raised to the power of {b} is {Math.Round(RaiseTo
 // 82 -> 10
 // 9012 -> 12
 
+/*
 int CountOfDigits(int num)
 {
     int res;
@@ -71,3 +72,33 @@ Console.Write("input a number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Sum of digits of the number {number} is {SumOfDigits(number)}");
 
+*/
+
+// Задача 29: Напишите программу, которая задаёт массив из m элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] CreateUsersArray (int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+        array[i] = Convert.ToInt32(Console.ReadLine());
+
+    return array; 
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + "  ");
+    Console.WriteLine();
+}
+
+Console.Write("input a size of the array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input elements of the array: ");
+
+int[] array = CreateUsersArray(size);
+Console.Write("created array: ");
+ShowArray(array);
