10854226fc33513a7e05b0cf037836ef0c554dd4
Natalia <nivanova86@bk.ru>
2022-11-23 02:04:02 +0300

    add 1st task

---
+++ HW8/Program.cs
@@ -0,0 +1,65 @@
+﻿//HW8
+// Задача 54: Задайте двумерный массив. Напишите программу, которая 
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+/*
+void GetSortedRowsArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        int sortedSize = 0;    
+        while(sortedSize < array.GetLength(1) - 1) // цикл полностью сортирует одну строку
+        {
+            for(int j = 0; j < array.GetLength(1) - 1 - sortedSize; j++) // цикл перемещает мин. эл-т в конец неотсортированной части массива
+            {
+                if(array[i, j] < array[i, j + 1])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = temp;
+                }
+            }
+            sortedSize++; // считает кол-во эл-тов в остсортированной части
+        }
+    }
+}
+
+Console.Write("Input a number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(myArray);
+
+GetSortedRowsArray(myArray);
+Show2dArray(myArray);
+*/
